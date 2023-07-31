8e454125861027b4ba05cbf67f04f2fbe7e1b1c4
Tina Ryu <tinaryu@tinas-mbp.myfiosgateway.com>
2023-07-31 16:01:59 -0400

    Adds search bar component and its stylings

---
+++ src/SearchBarComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./SearchBarComponent.css"
+
+export const SearchBarComponent = () => {
+    return (
+        <div className="search-input-wrapper">
+            <input placeholder="What do you want to see at the Met?"/>
+        </div>
+    )
+}
