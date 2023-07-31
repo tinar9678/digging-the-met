074bbdc9ef590c8082b421a1e7f06d9a29463bc8
Tina Ryu <tinaryu@tinas-mbp.myfiosgateway.com>
2023-07-31 18:57:36 -0400

    Added title and emoji

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { SearchBarComponent } from './SearchBarComponent';
 function App() {
   return (
     <div className="App">
+      <h1>&#127912; Virtual Met Gallery &#127912; </h1>
       <div className="search-area">
         <SearchBarComponent/>
         <div>SearchResults</div>
